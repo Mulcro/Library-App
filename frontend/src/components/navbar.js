c6457fa84c8d0e3e71bfe9e011087ccd983fc636
Mulero Alamou <Mulero.alamou@gmail.com>
2022-09-20 12:26:14 +0100

    initial commit

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <h1>Library App</h1>
+
+            <ul>
+                <li>Login</li>
+                <li>Home</li>
+                <li>Categories</li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Navbar;
