129790af11de743af41d36bae34d3abaf3de6ddc
Mulero Alamou <Mulero.alamou@gmail.com>
2022-09-22 18:56:17 +0100

    Created routes for home andbooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 // import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes as Switch} from 'react-router-dom';
 import Navbar from './components/navbar.js';
+import Sidebar from './components/sidebar.js';
+import Books from './components/books.js';
+import Home from './components/home.js';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Sidebar/>
+        <div className="display">
+          <Switch>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/books' element={<Books/>}/>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+
   );
 }
 
