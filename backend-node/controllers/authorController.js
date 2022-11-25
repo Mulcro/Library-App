1647fab6fea9c18300dc08b484f9aeb22c1881ea
Mulero Alamou <Mulero.alamou@gmail.com>
2022-11-25 01:18:22 +0100

    Added patch to controller

--- backend-node/controllers/authorController.js
+++ backend-node/controllers/authorController.js
@@ -37,13 +37,33 @@ const createAuthor = async (req,res) => {
 
 }
 
+const modifyAuthor = async(req,res) => {
+    if(!req.body.firstname || !req.body.lastname) return res.sendStatus(422);
+    const author = await Author.findById(req.params.authorId).exec();
+    author.firstname = req.body.firstname;
+    author.lastname = req.body.lastname;
+
+    const result = await author.save();
+    if(!result) return res.sendStatus(500);
+
+    res.json(result);
+
+}
+
 const deleteAuthor = async (req,res) => {
     const result1 = await Author.findByIdAndDelete(req.params.authorId).exec();
-    const result2 = await Book.findByIdAndDelete({author: req.params.authorId}).exec();
-    if(!result1 || !result2) return res.sendStatus(500);
+    const result2 = await Book.find({author: req.params.authorId}).exec();
 
-    res.json(result1,result2);
+    if(!result1 || !result2) return res.sendStatus(500);
+    
+    if(result2 != []){
+        for(const book of result2){
+            console.log(book);
+            book.deleteOne()
+        }
+    }
+    res.json(result1);
 }
 
 
-module.exports = {getAuthors,getBooksFromAuthor,getAuthor,createAuthor,deleteAuthor};
+module.exports = {getAuthors,getBooksFromAuthor,getAuthor,createAuthor,modifyAuthor,deleteAuthor};
