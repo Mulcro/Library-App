0e9e89f92afe84cc7574f4214be29f72e3efc650
Mulero Alamou <Mulero.alamou@gmail.com>
2022-11-25 01:23:31 +0100

    modified verify roles middleware to check for roles in body

--- backend-node/middleware/verifyRoles.js
+++ backend-node/middleware/verifyRoles.js
@@ -1,9 +1,10 @@
 const verifyRoles = (...allowedRoles) => {
     return (req,res,next) => {
-        if(!req?.roles) return res.sendStatus(401);
+        console.log(req.body.roles);
+        if(!req?.body.roles) return res.sendStatus(401);
 
         const authorizedRoles = [...allowedRoles];
-        const result = req.roles.map(role => authorizedRoles.includes(role)).find(val => val === true);
+        const result = req.body.roles.map(role => authorizedRoles.includes(role)).find(val => val === true);
         if(!result) return res.sendStatus(401);
         next();
     }
