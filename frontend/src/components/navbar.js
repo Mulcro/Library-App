458e1af2dd3323f1786088ad89cba5b71717da03
Mulero Alamou <Mulero.alamou@gmail.com>
2022-11-25 01:30:03 +0100

    added login/logout button to navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,11 +1,47 @@
-import {useState} from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { UserContext } from "../context/userContext";
+import BASE_URL from "../api/baseUrl";
 
 const Navbar = () => {
-    const [sidebar, setSidebar] = useState(false)
 
+    const {user, setUser} = useContext(UserContext);
+    const navigate = useNavigate();
+    const handleSignOut = (e) => {
+        e.preventDefault();
+
+        try{
+            console.log("Sign out");
+            fetch(BASE_URL + '/logout',{
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type":"application/json"
+                }
+            })
+            .then(res => {
+                console.log("Working");
+                res.json()
+            })
+            .then(() => {
+                setUser(null);
+                navigate("/");
+            });
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
     return ( 
         <div className="navbar">
             <h1>Library App</h1>
+
+            {!user &&
+               <Link to="login"><button className="login-logout">Login</button></Link>
+            }
+            {user &&
+                <Link><button className="login-logout" onClick={(e) => handleSignOut(e)}>Sign Out</button></Link>
+            }
         </div>
      );
 }
