b927409d2b2997f454a4c7870fd94aaf67b9f454
Mulero Alamou <Mulero.alamou@gmail.com>
2022-11-25 01:29:02 +0100

    created 404 component

---
+++ frontend/src/components/notFound.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return ( 
+        <section className="notFound">
+            <h1>The page you requested was not found :(</h1>
+        </section>
+     );
+}
+ 
+export default NotFound;
