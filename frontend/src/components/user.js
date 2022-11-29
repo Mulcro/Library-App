c64d31a38682f175b22f6c9cf471e057d299f55e
Mulero Alamou <Mulero.alamou@gmail.com>
2022-11-29 02:21:42 +0100

    Created User component

---
+++ frontend/src/components/user.js
@@ -0,0 +1,12 @@
+const User = () => {
+
+//Create this page showing books borrowed and other relevant info
+
+    return ( 
+        <section>
+
+        </section>
+     );
+}
+ 
+export default User;
