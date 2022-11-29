8c4580e5ec92acaf1f6354f490a4399e8c04a118
Mulero Alamou <Mulero.alamou@gmail.com>
2022-11-29 02:23:44 +0100

    added user icon to sidebar

--- frontend/src/data/sidebarData.js
+++ frontend/src/data/sidebarData.js
@@ -2,40 +2,47 @@ import * as GiIcons from 'react-icons/gi';
 import * as AiIcons from 'react-icons/ai';
 import * as BiIcons from 'react-icons/bi';
 import * as RiIcons from 'react-icons/ri';
+import * as FiIcons from 'react-icons/fi';
 
 export const SidebarData = [
     {
-        title: 'Home',
+        title: ' Home',
         path: '/',
         icon: <AiIcons.AiFillHome/>,
         cName: 'side-text'
     },
     {
-        title: 'All books',
+        title: ' All books',
         path: '/books',
         icon: <GiIcons.GiBookshelf/>,
         cName: 'side-text'
     },
     {
-        title: 'Categories',
+        title: ' Categories',
         path: '/categories',
         icon: <BiIcons.BiCategoryAlt/>,
         cName: 'side-text'
     },
     {
-        title: 'Authors',
+        title: ' Authors',
         path: '/authors',
         icon: <GiIcons.GiFountainPen/>,
         cName: 'side-text'
     },
     {
-        title: 'Search',
+        title: ' Search',
         path: '/search',
         icon: <GiIcons.GiRead/>,
         cName: 'side-text'
     },
     {
-        title: 'Admin',
+        title: " User",
+        path: "/user",
+        icon: <FiIcons.FiUser/>,
+        cName: "side-text"
+    },
+    {
+        title: ' Admin',
         path: '/admin',
         icon: <RiIcons.RiAdminFill/>,
         cName: 'side-text'
