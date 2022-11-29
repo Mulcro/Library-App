457fd6be26724f8e09d6f86e030f5ddeeeccdd5d
Mulero Alamou <Mulero.alamou@gmail.com>
2022-11-29 02:12:11 +0100

    Created boorrow controller for borrowing functionality

---
+++ backend-node/controllers/borrowController.js
@@ -0,0 +1,31 @@
+const Book = require("../model/Book");
+const User = require("../model/User");
+
+const borrowBook = async(req,res) => {
+    if(!req.body.username || !req.params.bookId) return res.sendStatus(422);
+
+    const book = await Book.findById(req.params.bookId).exec();
+    const user = await User.find({username: req.body.username});
+
+    if(!book || !user) return  res.sendStatus(404);
+    
+    User.findOneAndUpdate(
+        {username: req.body.username},
+        {$push: {books: req.params.bookId}},
+        (error,success) => {
+            if(error){
+                console.log(error);
+            }
+        }
+        );
+
+    book.quantity -= 1;
+    
+    const result2 = await book.save();
+
+    if(!result2) return res.sendStatus(500);
+
+    res.json(result2);
+}
+
+module.exports = {borrowBook}
