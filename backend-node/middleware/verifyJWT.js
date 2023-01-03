94d5b40f2728e9453d0b676c94ab9cf147853657
Mulero Alamou <Mulero.alamou@gmail.com>
2023-01-02 16:05:38 -0800

    Refactored code

--- backend-node/middleware/verifyJWT.js
+++ backend-node/middleware/verifyJWT.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const verifyJwt = (req,res,next) => {
     const authHeader = req.headers['authorization'];
+    console.log(authHeader);
     if(!authHeader) return res.sendStatus(401)
 
     const token = authHeader.split(' ');
@@ -9,12 +10,13 @@ const verifyJwt = (req,res,next) => {
     if(token[0] !== 'Bearer') return res.sendStatus(401);
 
     const authToken = token[1];
-
     jwt.verify(
         authToken,
         process.env.ACCESS_TOKEN,
         (err,decoded) => {
+            // console.log(authToken);
             if(err) return res.sendStatus(403);
+            
             req.user = decoded.userInfo.username;
             req.roles = decoded.userInfo.roles;
             next();
